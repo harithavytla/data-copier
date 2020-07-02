190f82fb1b995e8b50e95d890e4aefa52589237f
harithavytla <haritha.vytla@gmail.com>
2020-07-02 11:50:23 +0530

    Updated

--- config.py
+++ config.py
@@ -4,17 +4,18 @@ DB_DETAILS = {
     'dev': {
         'SOURCE_DB': {
             'DB_TYPE': 'mysql',
-            'DB_HOST': '139.99.209.131',
-            'DB_NAME': 'retail_db',
+            'DB_HOST': '34.70.54.149',
+            'DB_NAME': 'retail',
             'DB_USER': os.environ.get('SOURCE_DB_USER'),
-            'DB_PASS': os.environ.get('SOURCE_DB_PASS')
+            'DB_PASS': os.environ.get('SOURCE_DB_PASS'),
+
         },
         'TARGET_DB': {
             'DB_TYPE': 'postgres',
-            'DB_HOST': '139.99.209.131',
-            'DB_NAME': 'retail_db',
+            'DB_HOST': '34.70.54.149',
+            'DB_NAME': 'retail_dw',
             'DB_USER': os.environ.get('TARGET_DB_USER'),
-            'DB_PASS': os.environ.get('TARGET_DB_PASS')
+            'DB_PASS': os.environ.get('TARGET_DB_PASS'),
         }
     }
 }
